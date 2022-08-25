0966a7cd557f0e079082012310311417c8218349
DivergentClouds <27089165+DivergentClouds@users.noreply.github.com>
2022-08-24 19:09:52 -0500

    removed extrenuous versioning from portmidi.zig

--- src/portmidi.zig
+++ src/portmidi.zig
@@ -1,5 +1,5 @@
-// Requires PortMidi from June 28 2022 (103beb1)
-// https://github.com/PortMidi/portmidi/tree/103beb136ca9833d47d3f3a150364330d78638e7
+// Requires PortMidi
+// https://github.com/PortMidi/portmidi/
 //
 // Other versions may or may not work
 //
